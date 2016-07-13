bc514c16a3c8f4a6b13d1a1deed38e47b68f20fb
Akhmad Fauzi <Akhmad Fauzi>
2016-07-13 21:10:42 +0700

    initials commit

---
+++ README.md
@@ -0,0 +1,7 @@
+"# sale-backend"
+
+This is just simple sale-backend
+
+The features is:
+# Lavavel framework
+# angular ui using yoman
---
+++ api_ui/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name apiUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the apiUiApp
+ */
+angular.module('apiUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
